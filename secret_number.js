3955249375cfea64e1e615858d0ab4880d89672e
nick <nicnines3@gmail.com>
2018-01-02 16:43:55 -1000

    ksjdnfnsdf

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
 'use strict';
 module.exports = function() {
+    
+    let stored = Math.random() * 1000000;
+    let secretNumber = function() {
+        
+        
+        return stored;
+    }
+
+    return secretNumber ;
   
 };
+
